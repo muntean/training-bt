f45791bcbbaa00bf582326de2bff70057c6c84b1
Branko <branko-at-238386011931>
2022-05-10 13:56:39 +0300

    odoo academy day2 fields

--- odoo_academy/models/spaceship.py
+++ odoo_academy/models/spaceship.py
@@ -6,3 +6,11 @@ from odoo import api, fields, models, _
 class Spaceship(models.Model):
     _name = "academy.spaceship"
     _description = "Spaceship"
+
+    name = fields.Char(string='Name')
+    fuel_type = fields.Selection(string='Fuel Type', selection=[('lox', 'Liquid Oxygen'),
+                                                                ('lhy', 'Liquid Hydrogen')])
+    ship_type = fields.Selection(string='Ship Type', selection=[('carrier', 'Carrier'),
+                                                                ('fighter', 'Fighter'),
+                                                                ('destroyer', 'Destroyer')])
+    passengers = fields.Integer(string='Passengers No')
