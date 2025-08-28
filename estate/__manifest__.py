b1a344ca8ba168a51f1be2ac18234e90b0d021a0
branko <branko.tudorovici@gmail.com>
2025-08-28 10:04:24 +0300

    fix types offers smart button style + add properties in form views in res.users forms

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -21,6 +21,7 @@ Training Module
         'views/estate_property_offer_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
+        'views/res users_views.xml',
         'views/estate_menus.xml',
     ],
     'demo': [],
