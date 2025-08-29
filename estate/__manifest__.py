b3740857181fba5666eb81380631275a0f66a822
branko <branko.tudorovici@gmail.com>
2025-08-29 10:08:31 +0300

    res.users properties report and estate_account add report inheritance to add invoice info

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -23,6 +23,8 @@ Training Module
         'views/estate_property_tag_views.xml',
         'views/res users_views.xml',
         'views/estate_menus.xml',
+        'report/estate_property_templates.xml',
+        'report/estate_property_reports.xml',
     ],
     'demo': [],
     'css': [],
